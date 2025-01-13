62950c36e56065de01c1d424810b443e8c0c572b
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2025-01-13 23:12:51 +0900

    강의

--- src/main/java/com/thread/conditions/MainApplication.java
+++ src/main/java/com/thread/conditions/MainApplication.java
@@ -1,10 +1,13 @@
 package com.thread.conditions;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.StringJoiner;
 
 /**
  * packageName    : com.thread.conditions
@@ -18,10 +21,21 @@ import java.util.Scanner;
  * 25. 1. 10.        ipeac       최초 생성
  */
 public class MainApplication {
+    private static final String INPUT_FILE = "./out/matrices";
+    private static final String OUTPUT_FILE = "./out/matrices_results.txt";
     private static final int N = 10;
     
-    public static void main(String[] args) {
-    
+    public static void main(String[] args) throws IOException {
+        ThreadSafeQueue queue = new ThreadSafeQueue();
+        
+        File inputFile = new File(INPUT_FILE);
+        File outputFile = new File(OUTPUT_FILE);
+        
+        MatricsReaderProducer matricesReader = new MatricsReaderProducer(new FileReader(inputFile), queue);
+        MatricesMultiplierConsumer matricesMultiplier = new MatricesMultiplierConsumer(new FileWriter(outputFile), queue);
+        
+        matricesReader.start();
+        matricesMultiplier.start();
     }
     
     private static class MatricesMultiplierConsumer extends Thread {
@@ -34,6 +48,46 @@ public class MainApplication {
             this.queue = queue;
         }
         
+        @Override
+        public void run() {
+            while (true) {
+                final MatricesPair matricesPair = queue.remove();
+                if (matricesPair == null) {
+                    System.out.println("No more matrices to multiply, Consumer is terminating");
+                    break;
+                }
+                
+                float[][] result = multiplyMatrices(matricesPair.matrix1, matricesPair.matrix2);
+                
+                try {
+                    saveMatrixToFile(fileWriter, result);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            
+            try {
+                fileWriter.flush();
+                fileWriter.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        
+        private void saveMatrixToFile(FileWriter fileWriter, float[][] matrix) throws IOException {
+            for (int r = 0; r < N; r++) {
+                StringJoiner stringJoiner = new StringJoiner(", ");
+                for (int c = 0; c < N; c++) {
+                    stringJoiner.add(String.format("%.2f", matrix[r][c]));
+                }
+                
+                fileWriter.write(stringJoiner.toString());
+                fileWriter.write('\n');
+            }
+            
+            fileWriter.write('\n');
+        }
+        
         private float[][] multiplyMatrices(float[][] m1, float[][] m2) {
             float[][] result = new float[N][N];
             
@@ -44,6 +98,8 @@ public class MainApplication {
                     }
                 }
             }
+            
+            return result;
         }
     }
     
@@ -99,7 +155,18 @@ public class MainApplication {
         private boolean isEmpty = true;
         private boolean isTerminate = false;
         
+        //백프레셔 구현
+        private static final int CAPACITY = 5;
+        
         public synchronized void add(MatricesPair matricesPair) {
+            //while 을 통해 백프레셔 구현 => queue가 꽉 찼을 때는 wait()을 통해 대기
+            while (queue.size() == CAPACITY) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                }
+            }
+            
             queue.add(matricesPair);
             isEmpty = false;
             notify();
@@ -124,7 +191,13 @@ public class MainApplication {
             
             System.out.println("Queue size: " + queue.size());
             
-            return queue.remove();
+            final MatricesPair removed = queue.remove();
+            
+            if(queue.size() == CAPACITY - 1) {
+                notifyAll();
+            }
+            
+            return removed;
         }
         
         public synchronized void terminate() {
